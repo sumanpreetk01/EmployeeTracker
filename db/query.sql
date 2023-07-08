65ebae6397686e894f7d3187d1a11d53d5114e62
Sumanpreet Kaur <sharankaur@sharans-air.lan>
2023-07-08 09:02:54 -0400

    added code for viewAllDepartments and addEmployee

--- db/query.sql
+++ db/query.sql
@@ -7,14 +7,15 @@
 -- FROM role
 -- JOIN department ON role.department_id = department.id;
 
--- SELECT employee.id, employee.first_name, employee.last_name
--- FROM employee
--- JOIN employee manager ON employee.manager_id = manager_id;
+SELECT emp.id, CONCAT(emp.first_name, " ", emp.last_name) AS employee_name, role.title AS job_title, role.salary AS salary, department.name AS department, IFNULL(CONCAT(mans.first_name, " ", mans.last_name), "No Manager") AS manager
+FROM employee emp
+LEFT JOIN employee mans ON emp.manager_id = mans.id JOIN role ON emp.role_id = role.id JOIN department ON role.department_id = department.id
 
 -- View all employees
-SELECT role.id, first_name, last_name, title, name AS department, salary, manager_id
-FROM employee
-JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;
+-- SELECT role.id, first_name, last_name, title, name AS department, salary, manager_id
+-- FROM employee
+-- JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;
+
 
 -- View all departments
 -- SELECT *
--- server.js
+++ server.js
@@ -9,6 +9,72 @@ const questions = [{
     choices: ['View all employees', 'Add employee', 'Update employee role', 'View all roles', 'Add role', 'View all departments','Add department']
 }]
 
+const addDeptQuestions = ({
+    type:'input',
+    name:'deptName',
+    message:'What is the name of the department?'
+})
+
+const addRoleQuestions = (departmentOptions)=>[{
+    type:'input',
+    name:'roleName',
+    message:'What is the name of the role?'
+},{
+    type:'input',
+    name: 'salaryAmount',
+    message:'What is the salary of the role?'
+},{
+    type:'list',
+    name:'newRoleDept',
+    message: 'Which department does the role belong to?',
+    choices: departmentOptions.map((department)=>({
+        name:department.name,
+        value:department.id
+    }))
+}]
+
+const addEmplQuestions = (roleOptions, managerOptions)=>[{
+    type: 'input',
+    name: 'newFirstName',
+    message:`What is the employee's first name?`
+},
+{
+    type: 'input',
+    name: 'newLastName',
+    message:`What is the employee's last name?`
+},
+{
+    type: 'list',
+    name: 'newRole',
+    message:`What is the employee's role?`,
+    choices:roleOptions.map((role)=>({
+        name:role.title,
+        value:role.id
+    }))
+},
+{
+    type: 'list',
+    name: 'newManager',
+    message:`Who is the employee's manager?`,
+    choices: managerOptions
+}]
+
+const updateEmplQuestions = (employeeOptions,roleOptions)[{
+    type:'list',
+    name:'updatedEmployeeName',
+    message:`Which employee's role do you want to update?`,
+    choices:employeeOptions.map((employee)=>({
+        name:`${employee.first_name} ${employee.last_name}`
+    }))
+},
+{
+    type:'list',
+    name:'updatedEmployeeRole',
+    message:`Which role do you want to assign the selected employee?`,
+    choices:roleOptions
+}]
+
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -52,93 +118,98 @@ function init(){
     })
 }
 
-function viewAllEmployees(){
-    const query = `SELECT role.id, first_name, last_name, title, name AS department, salary, manager_id
-    FROM employee
-    JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id`;
+async function viewAllDepartments(){
+    try {
+       const query = await db.promise().query(`SELECT * FROM department`);
+        console.table(query[0])
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
+async function viewAllEmployees(){
+    try {
+       const query = await db.promise().query(`SELECT emp.id, CONCAT(emp.first_name, " ", emp.last_name) AS employee_name, role.title AS job_title, role.salary AS salary, department.name AS department, IFNULL(CONCAT(mans.first_name, " ", mans.last_name), "No Manager") AS manager
+        FROM employee emp
+        LEFT JOIN employee mans ON emp.manager_id = mans.id JOIN role ON emp.role_id = role.id JOIN department ON role.department_id = department.id`);
+        console.table(query[0])
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function addEmployee() {
+    try {
+      const [employeeRows, _] = await db.promise().query(`SELECT * FROM employee`);
+      const [roleRows, __] = await db.promise().query(`SELECT * FROM role`);
+  
+      const roleOptions = roleRows.map((role) => ({
+        id: role.id,
+        title: role.title,
+      }));
+  
+      const managerOptions = employeeRows.map((employee) => ({
+        value: employee.id,
+        name: `${employee.first_name} ${employee.last_name}`,
+      }));
+  
+      var employeeQuestions = addEmplQuestions(roleOptions, managerOptions);
+      const response = await inquirer.prompt(employeeQuestions);
+      console.log(response);
+      await db.promise().query(
+        `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+        [response.newFirstName, response.newLastName, response.newRole, response.newManager]
+      );
+      viewAllEmployees();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+async function updateEmployeeRole(){
+    try {
+        const [employeeRows,_] = await db.promise().query(`SELECT * FROM employee`);
+
+
+    } catch (error) {
+        
+    }
+}
+
+async function addRole(){
+    try {
+        const [departmentRows, _] = await db.promise().query("Select * FROM department")
+        var roleQuestions = addRoleQuestions(departmentRows)
+        const answers = await inquirer.prompt(roleQuestions)
+        console.log(answers)
+        await db.promise().query(`INSERT INTO role(title,salary,department_id) VALUES(?,?,?)`,[answers.roleName,answers.salaryAmount,answers.newRoleDept])
+        viewAllRoles()
+    } catch (error) {
+        console.log(error)
+    }
 };
 
-function addEmployee(){
-    const query = ``;
+async function addDepartment(){
+    try {
+        const response = await inquirer.prompt(addDeptQuestions)
+        await db.query(`INSERT INTO department(name) VALUES(?)`, [response.deptName]);
+        console.log('Department added successfully!')
+        console.table(response);
+        viewAllDepartments()
+    } catch (error) {
+        console.error(error)
+    }
 
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
-};
+    }
 
-function updateEmployeeRole(){
-    const query = ``;
-
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
-};
-
-function addRole(){
-    const query = ``;
-
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
-};
-
-function viewAllDepartments(){
-    const query = `SELECT *
-    FROM department`;
-
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
-};
-
-function addDepartment(){
-    const query = ``;
-
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
-};
-
-function viewAllRoles(){
-    const query = `SELECT role.id, title, name AS department, salary
-    FROM role
-    JOIN department ON role.department_id = department.id`;
-
-    db.query(query,(err,employees)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.table(employees)
-        };
-    });
+async function viewAllRoles(){
+    try {
+         const query = await db.promise().query(`SELECT role.id, title, name AS department, salary FROM role JOIN department ON role.department_id = department.id`);
+         console.table(query)
+    } catch (error) {
+        console.error(error)
+    }
 };
 
 init()
